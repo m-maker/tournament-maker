14ebffe44245d2521b492245f938b841bcb22b74
m-maker <damien.huctin@gmail.com>
2017-05-11 12:52:01 +0200

    fdt + css
    include('head.php)

---
+++ feuille_de_tournois.php
@@ -0,0 +1,37 @@
+<html>
+
+	<head>
+		<?php
+			include ('conf.php');
+		?>
+		<title>Tournoi</title>
+	</head>
+
+	<body>
+		<div id="menu_match">	
+			<ul class="nav nav-pills">
+	    		<li class="item_menu active"><a  data-toggle="tab" href="#Mur">Mur</a></li>
+	    		<li class="item_menu"><a class="" data-toggle="tab" href="#mon_equipe">Mon équipe</a></li>
+	    		<li class="item_menu"><a class="" data-toggle="tab" href="#equipes">Équipes</a></li>
+	  		</ul>
+	  	</div>	
+
+	  	<div id="body_match">
+			<div id="contenu_body_match" class="tab-content">
+	    		<div id="Mur" class="tab-pane fade in active">
+	    		
+	    		</div>
+
+	    		<div id="mon_equipe" class="tab-pane fade">
+	    			
+	    		</div>
+
+	    		<div id="equipes" class="tab-pane fade">
+	    			
+	    		</div>
+	    	</div>
+	    </div>
+	</body>
+</html>
+
+
--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <html>
 	
 	<head>
-		<link rel="stylesheet" href="css/bootstrap.css" />
-		<link rel="stylesheet" type="text/css" href="css/header.css">
-		<link rel="stylesheet" type="text/css" href="css/footer.css">
-		<link rel="stylesheet" type="text/css" href="css/style.css">
-		<script src="js/jquery.min.js"></script>
-		<script type="text/javascript" src="js/bootstrap.min.js"></script>
+		<?php include('head.php'); ?>
 		<title>Tournois de foot en salle</title>
 	</head>
 
